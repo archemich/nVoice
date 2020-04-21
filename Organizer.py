82483173e017264444ab697b03d29ef35e51ecf9
archemich <archemich@gmail.com>
2020-04-21 15:06:05 +0300

    Добавлены Таймер и Напоминалка

---
+++ Organizer.py
@@ -0,0 +1,27 @@
+import time
+import winsound
+import os
+import TextFromVoice as tts
+
+#Класс по работе с часами: Таймером, будильником и т.д
+class Clock:
+
+    #Класс таймера. На вход получает одно значение - количество минут до срабатывания таймера.
+    class Timer(object):
+        def __init__(self,minutes):
+            self.minutes = minutes
+            secs = minutes * 60
+            time.sleep(secs)
+            os.startfile(os.path.dirname(__file__) + "/audio/alarm.wav")
+
+    #Класс напоминалки. На вход получает время в минутах до срабатывания таймера и текст, о котором напоминает 
+    class Reminder (object):
+        def __init__(self,minutes,text):
+            self.minutes = minutes
+            self.text = text
+            Clock.Timer(minutes)
+            tts.getVoiceFromText(text)
+            time.sleep(1)
+            os.startfile(os.path.dirname(__file__) + "/audio/AudioResult.wav")
+
+Clock.Reminder(0.001,"Эээээээээээээээээээ")
