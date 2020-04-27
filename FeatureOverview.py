b216c18f2778766a427e0c4ded8e8b5d98d06c59
Hichigo112 <brownbear1@mail.ru>
2020-04-27 15:58:32 +0300

    Added func FeatureOverview

---
+++ FeatureOverview.py
@@ -0,0 +1,9 @@
+from TextFromVoice import getVoiceFromText
+from playAudio import play_audio
+
+# Обзор возможностей
+def OverView():
+    getVoiceFromText("Я могу найти что-либо в интернете, Сказать погоду,-- время и дату,-- температуру и влажность в комнате,-- записать текст в заметки,а также оповестить о заряде аккумулятора")
+    play_audio()
+
+OverView()
