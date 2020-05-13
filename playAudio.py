6710e8c1506b774780f7a4339092351e12e296e6
Pec3maker <apupro5@gmail.com>
2020-05-13 22:30:08 +0300

    Revert "fix"
    This reverts commit d7c8fb191f64c263ed33fb99bc6125b209b44608.

--- playAudio.py
+++ playAudio.py
@@ -9,26 +9,34 @@ def play_audio(name = None):
     if(name == None):
         name = "AudioResult"
 
-    wf = wave.open(os.path.dirname(__file__) + "/audio/" + name + ".wav", "rb")
-    p = pyaudio.PyAudio()
+    with wave.open(os.path.dirname(__file__) + "/audio/" + name + ".wav", "rb") as wf:
 
-    def callback(in_data, frame_count, time_info, status):
-        data = wf.readframes(frame_count)
-        return (data, pyaudio.paContinue)
+        # instantiate PyAudio (1)
+        p = pyaudio.PyAudio()
 
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=1,
-                    rate=48100,
-                    output=True,
-                    stream_callback=callback)
+        # define callback (2)
+        def callback(in_data, frame_count, time_info, status):
+            data = wf.readframes(frame_count)
+            return (data, pyaudio.paContinue)
 
-    stream.start_stream()
+        # open stream using callback (3)
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=1,
+                        rate=48100,
+                        output=True,
+                        stream_callback=callback)
 
-    while stream.is_active():
-        time.sleep(0.1)
+        # start the stream (4)
+        stream.start_stream()
 
-    stream.stop_stream()
-    stream.close()
-    wf.close()
+        # wait for stream to finish (5)
+        while stream.is_active():
+            time.sleep(0.1)
 
-    p.terminate()
+        # stop stream (6)
+        stream.stop_stream()
+        stream.close()
+        wf.close()
+
+        # close PyAudio (7)
+        p.terminate()
