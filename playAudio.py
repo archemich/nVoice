d7c8fb191f64c263ed33fb99bc6125b209b44608
Pec3maker <apupro5@gmail.com>
2020-05-13 17:15:02 +0300

    fix

--- playAudio.py
+++ playAudio.py
@@ -9,34 +9,26 @@ def play_audio(name = None):
     if(name == None):
         name = "AudioResult"
 
-    with wave.open(os.path.dirname(__file__) + "/audio/" + name + ".wav", "rb") as wf:
+    wf = wave.open(os.path.dirname(__file__) + "/audio/" + name + ".wav", "rb")
+    p = pyaudio.PyAudio()
 
-        # instantiate PyAudio (1)
-        p = pyaudio.PyAudio()
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
 
-        # define callback (2)
-        def callback(in_data, frame_count, time_info, status):
-            data = wf.readframes(frame_count)
-            return (data, pyaudio.paContinue)
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=1,
+                    rate=48100,
+                    output=True,
+                    stream_callback=callback)
 
-        # open stream using callback (3)
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=1,
-                        rate=48100,
-                        output=True,
-                        stream_callback=callback)
+    stream.start_stream()
 
-        # start the stream (4)
-        stream.start_stream()
+    while stream.is_active():
+        time.sleep(0.1)
 
-        # wait for stream to finish (5)
-        while stream.is_active():
-            time.sleep(0.1)
+    stream.stop_stream()
+    stream.close()
+    wf.close()
 
-        # stop stream (6)
-        stream.stop_stream()
-        stream.close()
-        wf.close()
-
-        # close PyAudio (7)
-        p.terminate()
+    p.terminate()
