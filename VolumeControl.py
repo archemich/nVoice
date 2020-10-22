bd888c6af06e08f051e8d3f3864ea0df446bff4a
archemich <archemich@gmail.com>
2020-10-22 12:28:34 +0300

    button control

--- VolumeControl.py
+++ VolumeControl.py
@@ -3,12 +3,13 @@ from playAudio import play_audio
 from TextFromVoice import getVoiceFromText
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-vol = 0
+GPIO.setmode(GPIO.BOARD)
+vol = 70
 VOLSTEP = 5
 def setVolume(text):
     text = ''.join(text).split()
     volume = int(text[text.index('на') + 1])
+    global vol
     vol = volume
     print(text)
     print(volume)
@@ -21,6 +22,7 @@ def setVolume(text):
         print("Нельзя поставить громкость на", (str(volume)))
        
 def changeVolume(volume):
+    global vol
     if volume >= 0 and volume <= 100:
         mix = Mixer()
         mix.setvolume(volume)
@@ -31,7 +33,9 @@ def changeVolume(volume):
     
 BUTTONUP = 29
 BUTTONDOWN = 31
+mix = Mixer()
+mix.setvolume(vol)
 GPIO.setup(BUTTONUP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTONDOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BUTTONUP, GPIO.FALLING, callback=changeVolume(vol+VOLSTEP), bouncetime=100)
-GPIO.add_event_detect(BUTTONDOWN, GPIO.FALLING, callback=changeVolume(vol-VOLSTEP), bouncetime=100)
+GPIO.add_event_detect(BUTTONUP, GPIO.FALLING, callback=partial(changeVolume, vol+VOLSTEP), bouncetime=100)
+GPIO.add_event_detect(BUTTONDOWN, GPIO.FALLING, callback=partial(changeVolume, vol-VOLSTEP), bouncetime=100)
